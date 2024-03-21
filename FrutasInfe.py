f9ef4e15b4642cabad578c062e2354d8a1602339
DiegoCampos20151718 <143902081+DiegoCampos20151718@users.noreply.github.com>
2024-03-21 06:03:26 -0600

    first commit

---
+++ FrutasInfe.py
@@ -0,0 +1,120 @@
+frutas_caracteristicas = {
+    "granadilla": {"tamaño": "mediano", "color": "naranja", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Sudamérica"},
+    "pomelo chino": {"tamaño": "grande", "color": "amarillo", "textura": "rugosa", "sabor": "ácido", "semillas": True, "originaria": "originaria del sudeste asiático"},
+    "zapote blanco": {"tamaño": "mediano", "color": "verde", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de América Central"},
+    "berenjena": {"tamaño": "grande", "color": "morado", "textura": "rugosa", "sabor": "amargo", "semillas": False, "originaria": "originaria de la India"},
+    "kiwi dorado": {"tamaño": "pequeño", "color": "amarillo", "textura": "peluda", "sabor": "dulce", "semillas": True, "originaria": "originaria de Nueva Zelanda"},
+    "pasa": {"tamaño": "pequeño", "color": "negro", "textura": "arrugada", "sabor": "dulce", "semillas": False, "originaria": "originaria de Grecia"},
+    "chayote": {"tamaño": "mediano", "color": "verde", "textura": "rugosa", "sabor": "neutro", "semillas": True, "originaria": "originaria de América Central"},
+    "platano macho": {"tamaño": "mediano", "color": "amarillo", "textura": "firme", "sabor": "neutro", "semillas": False, "originaria": "originaria del sudeste asiático"},
+    "uchuva": {"tamaño": "pequeño", "color": "amarillo", "textura": "firme", "sabor": "dulce", "semillas": True, "originaria": "originaria de América del Sur"},
+    "pepino dulce": {"tamaño": "mediano", "color": "amarillo", "textura": "crunchy", "sabor": "dulce", "semillas": True, "originaria": "originaria de América Central"},
+    "habanero": {"tamaño": "pequeño", "color": "naranja", "textura": "rugosa", "sabor": "picante", "semillas": True, "originaria": "originaria de América Central"},
+    "jitomate": {"tamaño": "mediano", "color": "rojo", "textura": "suave", "sabor": "neutro", "semillas": True, "originaria": "originaria de América Central"},
+    "durazno": {"tamaño": "pequeño", "color": "anaranjado", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de China"},
+    "mora": {"tamaño": "pequeño", "color": "morado", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Europa y Asia"},
+    "frambuesa": {"tamaño": "pequeño", "color": "rojo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Europa y Asia"},
+    "naranja": {"tamaño": "mediano", "color": "naranja", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria del sudeste asiático"},
+    "mandarina": {"tamaño": "mediano", "color": "naranja", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria del sudeste asiático"},
+    "platano": {"tamaño": "mediano", "color": "amarillo", "textura": "suave", "sabor": "dulce", "semillas": False, "originaria": "originaria del sudeste asiático"},
+    "kiwi": {"tamaño": "pequeño", "color": "verde", "textura": "peluda", "sabor": "agridulce", "semillas": True, "originaria": "originaria de China"},
+    "toronja": {"tamaño": "grande", "color": "rosa", "textura": "rugosa", "sabor": "agridulce", "semillas": True, "originaria": "originaria del sudeste asiático"},
+    "papaya": {"tamaño": "grande", "color": "anaranjado", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de América Central"},
+    "tuna": {"tamaño": "mediano", "color": "rojo", "textura": "espinosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de México"},
+    "mora azul": {"tamaño": "pequeño", "color": "azul", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Europa y Asia"},
+    "granada": {"tamaño": "mediano", "color": "rojo", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia"},
+    "ciruela": {"tamaño": "mediano", "color": "morado", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Europa y Asia"},
+    "jicama": {"tamaño": "grande", "color": "blanco", "textura": "firme", "sabor": "neutro", "semillas": False, "originaria": "originaria de México"},
+    "fresa": {"tamaño": "pequeño", "color": "rojo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Europa y America del norte"},
+    "coco": {"tamaño": "grande", "color": "marrón", "textura": "firme", "sabor": "dulce", "semillas": False, "originaria": "originaria de Asia tropical"},
+    "lichi": {"tamaño": "pequeño", "color": "rojo", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de China"},
+    "zarzamora": {"tamaño": "pequeño", "color": "morado", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Europa y Asia"},
+    "higo": {"tamaño": "mediano", "color": "morado", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia occidental"},
+    "lima": {"tamaño": "pequeño", "color": "amarillo", "textura": "rugosa", "sabor": "ácido", "semillas": True, "originaria": "originaria del sudeste asiático"},
+    "pitaya": {"tamaño": "mediano", "color": "rosa", "textura": "escamosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de México y América Central"},
+    "fruta estrella": {"tamaño": "mediano", "color": "amarillo", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Malasia e Indonesia"},
+    "tamarindo": {"tamaño": "pequeño", "color": "marrón", "textura": "rugosa", "sabor": "agridulce", "semillas": True, "originaria": "originaria de África tropical"},
+    "chirimoya": {"tamaño": "mediano", "color": "verde", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de los Andes"},
+    "grosella": {"tamaño": "pequeño", "color": "rojo", "textura": "suave", "sabor": "agridulce", "semillas": True, "originaria": "originaria de Europa y Asia"},
+    "guanábana": {"tamaño": "grande", "color": "verde", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de América tropical"},
+    "tejocote": {"tamaño": "pequeño", "color": "amarillo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de México"},
+    "níspero": {"tamaño": "mediano", "color": "anaranjado", "textura": "suave", "sabor": "agridulce", "semillas": True, "originaria": "originaria de China"},
+    "carambola": {"tamaño": "mediano", "color": "amarillo", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Malasia e Indonesia "},
+    "piña": {"tamaño": "grande", "color": "amarillo", "textura": "espinosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de América tropical"},
+    "rambután": {"tamaño": "pequeño", "color": "rojo", "textura": "peluda", "sabor": "dulce", "semillas": True, "originaria": "originaria de Malasia e Indonesia"},
+    "cereza": {"tamaño": "pequeño", "color": "rojo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Europa y Asia"},
+    "chavacanos": {"tamaño": "mediano", "color": "naranja", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia tropical"},
+    "ciruelas": {"tamaño": "mediano", "color": "morado", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Europa y Asia"},
+    "pera": {"tamaño": "mediano", "color": "amarillo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Europa y Asia"},
+    "manzana": {"tamaño": "mediano", "color": "rojo", "textura": "firme", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia occidental"},
+    "nectarina": {"tamaño": "pequeño", "color": "naranja", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia occidental"},
+    "mamey": {"tamaño": "mediano", "color": "marron", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de México y América Central"},
+    "dátil": {"tamaño": "pequeño", "color": "marrón", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria del Medio Oriente"},
+    "salak": {"tamaño": "pequeño", "color": "marrón", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria del sudeste asiático"},
+    "lulo": {"tamaño": "grande", "color": "amarillo", "textura": "rugosa", "sabor": "agridulce", "semillas": True, "originaria": "originaria de Colombia y Ecuador"},
+    "pitanga": {"tamaño": "pequeño", "color": "rojo", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Brasil"},
+    "lucuma": {"tamaño": "mediano", "color": "verde", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de los Andes"},
+    "longan": {"tamaño": "pequeño", "color": "amarillo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria del sudeste asiático"},
+    "arandano": {"tamaño": "pequeño", "color": "azul", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de América del Norte"},
+    "limon": {"tamaño": "pequeño", "color": "verde", "textura": "rugosa", "sabor": "ácido", "semillas": True, "originaria": "originaria del sudeste asiático"},
+    "melon": {"tamaño": "grande", "color": "rosa", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia"},
+    "sandia": {"tamaño": "grande", "color": "verde", "textura": "crunchy", "sabor": "dulce", "semillas": True, "originaria": "originaria de África"},
+    "almendra": {"tamaño": "pequeño", "color": "marrón", "textura": "firme", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia"},
+    "mango": {"tamaño": "mediano", "color": "amarillo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia tropical"},
+    "maracuya": {"tamaño": "mediano", "color": "amarillo", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Sudamérica"},
+    "aguacate": {"tamaño": "grande", "color": "verde", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de México"},
+    "ejote": {"tamaño": "mediano", "color": "verde", "textura": "firme", "sabor": "neutro", "semillas": True, "originaria": "originaria de México"},
+    "pepino": {"tamaño": "mediano", "color": "verde", "textura": "crunchy", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia tropical"},
+    "lychee": {"tamaño": "mediano", "color": "rojo", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de China"},
+    "chile serrano": {"tamaño": "pequeño", "color": "verde", "textura": "rugosa", "sabor": "picante", "semillas": True, "originaria": "originaria de México"},
+    "membrillo": {"tamaño": "mediano", "color": "amarillo", "textura": "firme", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia"},
+    "capulin": {"tamaño": "pequeño", "color": "rojo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de América"},
+    "tamarillo": {"tamaño": "mediano", "color": "rojo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de América del Sur"},
+    "bergamota": {"tamaño": "mediano", "color": "amarillo", "textura": "rugosa", "sabor": "ácido", "semillas": True, "originaria": "originaria de 	Europa"},
+    "kiwano": {"tamaño": "mediano", "color": "amarillo", "textura": "espinosa", "sabor": "agridulce", "semillas": True, "originaria": "originaria de África"},
+    "akebia": {"tamaño": "mediano", "color": "morado", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia"},
+    "yaca": {"tamaño": "grande", "color": "verde", "textura": "rugosa", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia tropical"},
+    "nashi": {"tamaño": "mediano", "color": "amarillo", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia"},
+    "pineberry": {"tamaño": "pequeño", "color": "blanco", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Sudamérica"},
+    "nuez": {"tamaño": "pequeño", "color": "marrón", "textura": "firme", "sabor": "dulce", "semillas": True, "originaria": "originaria de América del Norte"},
+    "chabacano": {"tamaño": "mediano", "color": "naranja", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia"},
+    "zapote negro": {"tamaño": "grande", "color": "negro", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de América"},
+    "uva": {"tamaño": "pequeño", "color": "morado", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Asia, Europa y América"},
+    "tangelo": {"tamaño": "mediano", "color": "naranja", "textura": "suave", "sabor": "dulce", "semillas": True, "originaria": "originaria de Florida, Estados Unidos"}
+}
+
+def identificar_fruta():
+    print("Responde las siguientes preguntas para identificar la fruta:")
+    tiene_semillas = input("¿Tiene la fruta semillas? (si/no) ").lower()
+    color = input("¿De qué color es la fruta? ").lower()
+    textura = input("¿Cómo es la textura de la fruta? (rugosa/suave/crunchy/arrugada/firme/peluda/espinosa/escamosa) ").lower()
+    sabor = input("¿Cómo es el sabor de la fruta? (dulce/agridulce/amargo/neutro/picante/ácido) ").lower()
+    tamaño = input("¿Cual es el tamaño de la fruta? (pequeño (1cm a 7cm), mediano (8 a 15 cm) o grande (16 cm en adelante)) ").lower()
+
+    frutas_coincidentes = []
+
+    for fruta, caracteristicas in frutas_caracteristicas.items():
+        if (tiene_semillas == "si" and caracteristicas["semillas"] or tiene_semillas == "no" and not caracteristicas["semillas"]) and \
+                caracteristicas["color"] == color and caracteristicas["textura"] == textura and caracteristicas["sabor"] == sabor and caracteristicas["tamaño"] == tamaño:
+            respuesta = input(f"¿Es {fruta} la fruta que estás pensando? (si/no) ").lower()
+            if respuesta == "si":
+                return fruta, caracteristicas["originaria"]
+        elif (tiene_semillas == "si" and caracteristicas["semillas"] or tiene_semillas == "no" and not caracteristicas["semillas"]) and \
+                caracteristicas["color"] == color and caracteristicas["textura"] == textura and caracteristicas["sabor"] == sabor:
+            frutas_coincidentes.append(fruta)
+
+    if frutas_coincidentes:
+        print("No se pudo identificar la fruta.")
+        print("Frutas que coinciden con las características proporcionadas:")
+        for fruta in frutas_coincidentes:
+            print(fruta)
+    else:
+        print("No se pudo identificar la fruta.")
+
+    return None, None
+
+# Llamada a la función
+fruta_identificada, origen = identificar_fruta()
+if fruta_identificada:
+    print("La fruta identificada es:", fruta_identificada)
+    print("Origen de la fruta:", origen)
